2f9b7dd255996c2795cfd16c42f5a64954542173
Selvin Norman <selvin715@gmail.com>
2018-10-11 14:32:15 -0700

    Sorting books Done

--- DSA-Sorting-drills.js
+++ DSA-Sorting-drills.js
@@ -85,6 +85,23 @@ function sortInPlace(array) {
   console.log(array);
 }
 
+
+
+//Sorting books
+//  Create an object
+//  Map through the book titles array
+//  Compute a hash value by multiplying each letter's index+1 by the ascii code of the letter 
+//  Add all the resulting values together to create a hash. 
+//  Add key/value pair to object, where the key is the hash and the value is the title.
+//  Return the hash.
+//  When the mapping is done, you will have an array of hashes and an object of key value pairs.
+//  Sort the hashes array with qSort.
+//  Map the sorted hashes array.
+//  For each hash in the array, return obj[hash]
+//  Now the sorted hashes array is a sorted titles array.
+//  Return the array.
+
+
 function main() {
   const array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
   // qSort(array);
