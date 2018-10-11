b47003dabde35eb05cfcdd6cd5144210196b2877
Selvin Norman <selvin715@gmail.com>
2018-10-11 13:05:56 -0700

    count number of operations

--- DSA-Sorting-drills.js
+++ DSA-Sorting-drills.js
@@ -1,6 +1,8 @@
 'use strict';
-
+let mCounter = 0;
+let qCounter = 0;
 function qSort (array, start=0, end=array.length){
+  qCounter++;
   //base case
   if (start >= end) {
     return array;
@@ -25,12 +27,14 @@ function partition(array, start, end) {
 }
 
 function swap(array, i, j) {
+  qCounter++;
   let tmp = array[i];
   array[i] = array[j];
   array[j] = tmp;
 }
 
 function mSort(array) {
+  mCounter++;
   if (array.length <= 1) {
     return array;
   }
@@ -46,6 +50,7 @@ function mSort(array) {
 }
 
 function merge(left, right, array) {
+  mCounter++;
   let leftIndex = 0;
   let rightIndex = 0;
   let outputIndex = 0;
@@ -75,7 +80,10 @@ function main() {
   const array = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
   // qSort(array);
   // console.log(array);
+
   mSort(array);
   console.log(array);
+  console.log('Operations count: ', mCounter);
+
 }
 main();
